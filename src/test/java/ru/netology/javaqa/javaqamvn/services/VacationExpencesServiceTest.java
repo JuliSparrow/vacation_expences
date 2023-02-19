e3bbfe34119628054d3ce4cc2a69d2d9dd8732d2
QA <feverx@inbox.ru>
2023-02-19 23:38:16 +0300

    добавление данных из csv

--- src/test/java/ru/netology/javaqa/javaqamvn/services/VacationExpencesServiceTest.java
+++ src/test/java/ru/netology/javaqa/javaqamvn/services/VacationExpencesServiceTest.java
@@ -1,27 +1,14 @@
 package ru.netology.javaqa.javaqamvn.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class VacationExpencesServiceTest {
-    @Test
-    public void shouldCalculateVacationAmount() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/money.csv")
+    public void shouldCalculateVacationAmount(int income, int expenses, int threshold, int expected) {
         VacationExpencesService service = new VacationExpencesService();
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-        int actual = service.calculate(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateAnotherVacationAmount() {
-        VacationExpencesService service = new VacationExpencesService();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
         int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
